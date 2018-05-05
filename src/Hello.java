b7b986df03950229e6e971cc2f4eb33527b3729e
Taylor Lassiter <lassit09@gmail.com>
2018-05-05 07:15:24 -0400

    add notes about syntax and what keywords mean

--- src/Hello.java
+++ src/Hello.java
@@ -1,8 +1,13 @@
 public class Hello {
+    /* public: anyone can access it
+       static: method can be run without creating an instance of hte class containing the main method
+       void: method doesn't return any value
+       main: the name of the method
+    */
     public static void main(String[] args) {
         System.out.println("Hello, World!");
 
-        /* store an integer called myFirstNumber and assign value of 5 to it */
+        // store an integer called myFirstNumber and assign value of 5 to it
         int myFirstNumber = 5;
         int mySecondNumber = 12;
         int myThirdNumber = myFirstNumber * 2;
